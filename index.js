ebe4f3334d157a4718418fcc601a228ee631bfeb
zethboy <yoginugraha842@gmail.com>
2024-12-27 23:39:14 +0700

    arraycoii

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
-// let usia = prompt('masukkan tahun anda lahir')
+// // let usia = prompt('masukkan tahun anda lahir')
 
-//  usia = 2024 - usia
+// //  usia = 2024 - usia
 
-// console.log('heyhoo usia kamu adalah ' + usia )
-// alert('usia kamu adalah ' + usia)
-//=================================
+// // console.log('heyhoo usia kamu adalah ' + usia )
+// // alert('usia kamu adalah ' + usia)
+// //=================================
 
-let nama = 'yogi nugraha' //string
-let usia = 19              // integer
-let tinggiBadan = 174.5 // double float
-let beratBadan
-let pacar = 3 // null = tidak bernilai
+// let nama = 'yogi nugraha' //string
+// let usia = 19              // integer
+// let tinggiBadan = 174.5 // double float
+// let beratBadan
+// let pacar = 3 // null = tidak bernilai
 
-beratBadan = 60
+// beratBadan = 60
 
-// if(pacar == null ){
-//     pacar = 'belum punya'
+// // if(pacar == null ){
+// //     pacar = 'belum punya'
+// // }
+// // else{
+// //     pacar = 'udah punya'
+// // }
+// //=============================
+
+// switch(pacar){
+//     case 1:
+//         pacar = 'punya satu aja'
+//     break
+//     case 2:
+//         pacar = 'mayan'
+//     break
+//     case 3 :
+//         pacar = 'banyak coii'
+//     break
+//     default:
+//         pacar = 'belum punya'
+//     break
 // }
-// else{
-//     pacar = 'udah punya'
-// }
-//=============================
 
-switch(pacar){
-    case 1:
-        pacar = 'punya satu aja'
-    break
-    case 2:
-        pacar = 'mayan'
-    break
-    case 3 :
-        pacar = 'banyak coii'
-    break
-    default:
-        pacar = 'belum punya'
-    break
+
+// alert(`nama saya ${nama} usia saya ${usia} tinggi badan saya ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
+
+// buat array
+const arraySaya = ['Yogi', 'Aya']
+console.log(arraySaya)
+arraySaya.push ('nadine')
+console.log(arraySaya)
+
+if (arraySaya.includes('nadine')){
+    const posisi = arraySaya.indexOf('nadine')
+    console.log(`posisi Aya ada di index ke ${posisi}`)
 }
-
-
-alert(`nama saya ${nama} usia saya ${usia} tinggi badan saya ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
+else {
+    console.log(`Aya tidak ada`)
+}
