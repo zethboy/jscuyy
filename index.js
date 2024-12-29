2dcff4d5d21a534f312a7f17b985a190d2de92f5
zethboy <yoginugraha842@gmail.com>
2024-12-29 13:31:33 +0700

    array kopi john mayer

--- index.js
+++ index.js
@@ -41,22 +41,42 @@
 // alert(`nama saya ${nama} usia saya ${usia} tinggi badan saya ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
 
 // buat array
-const arraySaya = ['Yogi', 'Aya']
-console.log(arraySaya)
-arraySaya.push ('nadine')
-console.log(arraySaya)
+// const arraySaya = ['Yogi', 'Aya']
+// console.log(arraySaya)
+// arraySaya.push ('nadine')
+// console.log(arraySaya)
 
-if (arraySaya.includes('Aya')){
-    const posisi = arraySaya.indexOf('Aya')
-    const before = posisi - 1
-    const after = posisi + 1
-    const posisiBefore = arraySaya[before]
-    const posisiAfter = arraySaya[after]
+// if (arraySaya.includes('Aya')){
+//     const posisi = arraySaya.indexOf('Aya')
+//     const before = posisi - 1
+//     const after = posisi + 1
+//     const posisiBefore = arraySaya[before]
+//     const posisiAfter = arraySaya[after]
 
-    console.log(`posisi sebelum aya ${posisiBefore}`)
-    console.log(`posisi sesudah aya ${posisiAfter}`)
-    console.log(`posisi Aya ada di index ke ${posisi}`)
-}
-else {
-    console.log(`Aya tidak ada`)
-}
+//     console.log(`posisi sebelum aya ${posisiBefore}`)
+//     console.log(`posisi sesudah  aya ${posisiAfter}`)
+//     console.log(`posisi Aya ada di index ke ${posisi}`)
+// }
+// else {
+//     console.log(`Aya tidak ada`)
+// }
+
+// const arrySaya = [] 
+
+// arrySaya[0] = 'manggo'
+// arrySaya[1] = 'avocado'
+// arrySaya[2] = 'watermelon'
+
+// console.log(arrySaya)
+
+// const arraySaya = [1, 2, 3, 4, 5, 6, 7]
+// const newArraySaya = [...arraySaya]
+// newArraySaya[1] = 9
+// console.log(arraySaya)
+// console.log(newArraySaya)
+
+// const arraySaya = ['yogi', 2, {allea : true}, {renata : function(){ console.log('renata lopyu')},}, ['aya', 'respa']]
+// arraySaya[3].renata()
+
+// const arraySaya = ['yogi', 2, {allea : true}, {renata : function(){ console.log('renata lopyu')},}, ['aya', 'respa']]
+// console.log(arraySaya[4][1])
