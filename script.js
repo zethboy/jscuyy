1ee2049a09aeea8d83cdcccbdba3c9e5a61115b2
zethboy <yoginugraha842@gmail.com>
2025-01-25 22:00:19 +0700

    test bro

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ header.style.backgroundColor = 'grey'
 header.style.textAlign = 'center'
 header.innerHTML = 'YOGI NUGRAHA'
 
+header.style.fontSize = '60px'
 
 const p = document.getElementsByTagName('p')
 for (let i = 0; i < p.length; i++){
   p[i].style.backgroundColor = 'yellow'
 }
+
