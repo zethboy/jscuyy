f02e2c6420af9ff3c571cfef84a1acb650a2cb0c
zethboy <yoginugraha842@gmail.com>
2025-01-31 23:03:35 +0700

    belajar ganti node

--- latnod.js
+++ latnod.js
@@ -22,5 +22,15 @@ sectionB.insertBefore(liBaru, liLok)
 // remove link ig
 
 const link = document.getElementsByTagName('a')[0]
-
 sectionA.removeChild(link)
+
+// mengganti node
+const secB = document.querySelector('#b')
+const p4 = secB.querySelector('p:nth-child(1)') //bisa juga p doang karen p4 ada di indek ke 1
+
+const h2baru = document.createElement('h2')
+const texth2b = document.createTextNode('belajar ganti node')
+
+h2baru.appendChild(texth2b)
+
+secB.replaceChild(h2baru, p4)
