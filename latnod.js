54815d1a06ed7201b21f9b417f33d7afe3a8c4c8
zethboy <yoginugraha842@gmail.com>
2025-01-31 22:46:42 +0700

    dom node

--- latnod.js
+++ latnod.js
@@ -5,6 +5,15 @@ const textPB = document.createTextNode('paragraf baru')
 // simpan tulisan ke dalam paragraf
 pBaru.appendChild(textPB)
 // tempatkan dimana si pb ini (disimpan di  akhir section a)
-
 const sectionA = document.querySelector('#a')
 sectionA.appendChild(pBaru)
+
+// membuat li baru
+const liBaru = document.createElement('li')
+const textLiBaru = document.createTextNode('item 1,5')
+// simpan tulisan ke li
+liBaru.appendChild(textLiBaru)
+// mau disimpen dimana (abis item 1 section b)
+const sectionB = document.querySelector('section#b ul')
+const liLok = sectionB.querySelector('li:nth-child(2)')
+sectionB.insertBefore(liBaru, liLok)
