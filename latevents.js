d1d6743b04ae81e769b6726031540a8614e2b41f
zethboy <yoginugraha842@gmail.com>
2025-02-01 21:56:20 +0700

    event handler in script

--- latevents.js
+++ latevents.js
@@ -1,4 +1,7 @@
 const p3 = document.querySelector('.p3')
 function ubahwarna() {
-  p3.style.backgroundColor = 'red' 
+  p2.style.backgroundColor = 'red' 
 }
+
+const p2 = document.querySelector('.p2')
+p2.onclick = ubahwarna
