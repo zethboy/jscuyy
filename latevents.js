6299a12683f16ddfaf78a1c1ec5448b3c6c4a87b
zethboy <yoginugraha842@gmail.com>
2025-02-02 22:21:26 +0700

    absen

--- latevents.js
+++ latevents.js
@@ -1,7 +1,11 @@
 const p3 = document.querySelector('.p3')
-function ubahwarna() {
-  p2.style.backgroundColor = 'red' 
+function ubahwarna3() {
+  p3.style.backgroundColor = 'red' 
 }
 
 const p2 = document.querySelector('.p2')
+function ubahwarna() {
+  p2.style.backgroundColor = 'yellow' 
+}
 p2.onclick = ubahwarna
+
