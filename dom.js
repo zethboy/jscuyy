cf91a54149ed2eac7292e6a6d6a5fa77d97f62b2
zethboy <yoginugraha842@gmail.com>
2025-02-04 21:39:05 +0700

    kustom warna

--- dom.js
+++ dom.js
@@ -18,6 +18,24 @@ tRandom.addEventListener('click', function(){
 })
 
 const sMerah = document.querySelector('input[name = sMerah]')
+const sHijau = document.querySelector('input[name = sHijau]')
+const sBiru = document.querySelector('input[name = sBiru]')
+
 sMerah.addEventListener('change', function(){
-  
+  const r = sMerah.value
+  const g = sHijau.value
+  const b = sBiru.value
+  document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')'
+})
+sHijau.addEventListener('change', function(){
+  const r = sMerah.value
+  const g = sHijau.value
+  const b = sBiru.value
+  document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')'
+})
+sBiru.addEventListener('change', function(){
+  const r = sMerah.value
+  const g = sHijau.value
+  const b = sBiru.value
+  document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')'
 })
