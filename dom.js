5b41aafb211e7366647bcb472b633945103d1eee
zethboy <yoginugraha842@gmail.com>
2025-02-04 14:44:02 +0700

    math random

--- dom.js
+++ dom.js
@@ -3,3 +3,19 @@ tombolcuy.addEventListener('click', function(){
   // document.body.style.backgroundColor = 'orange'
   document.body.classList.toggle('tombol')
 })
+
+const tRandom = document.createElement('button')
+const textT = document.createTextNode('acak warna')
+tRandom.appendChild(textT)
+tRandom.setAttribute('type', 'button')
+tombolcuy.after(tRandom)
+
+tRandom.addEventListener('click', function(){
+  const r = Math.round(Math.random() * 255 + 1)
+  const g = Math.round(Math.random() * 255 + 1)
+  const b = Math.round(Math.random() * 255 + 1)
+
+  document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')'
+
+
+})
