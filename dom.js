680efe2253f2b9d16660e203066f194591d13380
zethboy <yoginugraha842@gmail.com>
2025-02-04 21:21:22 +0700

    ambil nilai

--- dom.js
+++ dom.js
@@ -14,8 +14,10 @@ tRandom.addEventListener('click', function(){
   const r = Math.round(Math.random() * 255 + 1)
   const g = Math.round(Math.random() * 255 + 1)
   const b = Math.round(Math.random() * 255 + 1)
-
   document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')'
+})
 
-
+const sMerah = document.querySelector('input[name = sMerah]')
+sMerah.addEventListener('change', function(){
+  
 })
