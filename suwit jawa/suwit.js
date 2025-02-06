8cc790c4924f8081ccadd9e8f04718d0dcd1009e
zethboy <yoginugraha842@gmail.com>
2025-02-06 22:16:13 +0700

    logik

--- suwit jawa/suwit.js
+++ suwit jawa/suwit.js
@@ -6,10 +6,25 @@ function getPilKom(){
     return 'semut';
 }
 
-function getRules(comp, user){
-  if( user == comp ) return 'SERI!';
-   if( user == 'gajah' ) return ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
-   if( user == 'orang' ) return ( comp == 'gajah' ) ? 'KALAH!' : 'MENANG!';
-   if( user == 'semut' ) return ( comp == 'orang' ) ? 'KALAH' : 'MENANG!';
-   return 'memasukkan pilihan yang salah!';
+function getRules(comp, user) {
+  if (user == comp) return 'SERI!';
+  if (user == 'gajah') return (comp == 'orang') ? 'MENANG!' : 'KALAH!';
+  if (user == 'orang') return (comp == 'gajah') ? 'KALAH!' : 'MENANG!';
+  if (user == 'semut') return (comp == 'orang') ? 'KALAH!' : 'MENANG!';
+  
+  return 'Pilihan tidak valid!';
 }
+
+
+const pGajah = document.querySelector('.gajah')
+const pSemut = document.querySelector('.semut')
+const pOrang = document.querySelector('.orang')
+
+pGajah.addEventListener('click', function(){
+  const pilkom = getPilKom()
+  const pilPlay = pGajah.className
+  const hasil = getRules(pilkom, pilPlay)
+  console.log('komputer = ' + pilkom)
+  console.log('player = ' + pilPlay)
+  console.log('hasil = ' + hasil)
+})
