7f6b4ca3a25f2fe57e2af166a589fe8e8e78e79f
zethboy <yoginugraha842@gmail.com>
2025-02-08 21:49:25 +0700

    dom traversal

--- telusurdom/tlusur.js
+++ telusurdom/tlusur.js
@@ -1,5 +1,16 @@
-const card = document.querySelector('.card')
-const hilang = document.querySelector('.close')
-hilang.addEventListener('click', function(){
-  card.style.display = "none"
-})
+// const card = document.querySelectorAll('.card')
+// const hilang = document.querySelectorAll('.close')
+// hilang.addEventListener('click', function(){
+//   card.style.display = "none"
+// })
+
+// dom traversal
+
+const hilang = document.querySelectorAll('.close')
+
+for (let i = 0; i < hilang.length; i++){
+  hilang[i].addEventListener('click', function(){
+    hilang[i].parentElement.style.display = 'none'
+    
+  })
+}
