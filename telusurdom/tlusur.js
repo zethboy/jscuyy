f19af41281ba211e38274ee36cfc6fac5e4b83ad
zethboy <yoginugraha842@gmail.com>
2025-02-09 21:40:30 +0700

    test

--- telusurdom/tlusur.js
+++ telusurdom/tlusur.js
@@ -17,14 +17,15 @@ const hilang = document.querySelectorAll('.close')
 
 hilang.forEach(function(el){
   el.addEventListener('click', function(e){
-    e.target.parentElement.style.display = 'none'
+    e.target.parentElement.parentElement.style.display = 'none'
   })
 })
 
-const nama = document.querySelectorAll('.nama')
-for(let i = 0; i < nama.length; i++){
-nama[i].addEventListener('click', function(na){
+const nama = document.querySelector('.nama')
+// for(let i = 0; i < nama.length; i++){
+// nama[i].addEventListener('click', function(na){
   
-  na.target.nextElementSibling.style.color = 'red'
-})
-}
+//   na.target.nextElementSibling.style.color = 'red'
+// })
+// }
+// console.log(nama.parentElement.parentElement);
