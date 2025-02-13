cbb05cded73aae80837883c22f11dc501655cddc
zethboy <yoginugraha842@gmail.com>
2025-02-13 21:58:47 +0700

    test

--- imggalery/img.js
+++ imggalery/img.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container')
 const jumbo = document.querySelector('.cat1')
+const thumbs = document.querySelectorAll('.cat')
 
 
 container.addEventListener('click', function(e){
@@ -9,5 +10,13 @@ container.addEventListener('click', function(e){
     setTimeout(function(){
       jumbo.classList.remove('fade');
     }, 500)
+    thumbs.forEach(function(thumb){
+      // if (thumb.classList.contains('active')){
+      //   thumb.classList.remove('active')
+      // }
+      // atau
+      thumb.className = 'cat'
+    })
+    e.target.classList.add('active')
   }
 }) 
