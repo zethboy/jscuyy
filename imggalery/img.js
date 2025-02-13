4c5726822ea4c6bda1a99e0723aaa2e337440179
zethboy <yoginugraha842@gmail.com>
2025-02-13 21:48:46 +0700

    key frames

--- imggalery/img.js
+++ imggalery/img.js
@@ -5,5 +5,9 @@ const jumbo = document.querySelector('.cat1')
 container.addEventListener('click', function(e){
   if (e.target.className == 'cat' ){
     jumbo.src = e.target.src;
+    jumbo.classList.add('fade');
+    setTimeout(function(){
+      jumbo.classList.remove('fade');
+    }, 500)
   }
 }) 
