b1ae34bdc58220e8e07a176063052f111b856625
zethboy <yoginugraha842@gmail.com>
2025-02-26 21:08:32 +0700

    fungsi deklarasi

--- js lanjutan/index.js
+++ js lanjutan/index.js
@@ -1,25 +1,39 @@
 // CARA BUAT OBJEK DI JAVASCRIPT 
 // OBJEK LITERAL
 
-let mahasiswa1 = {
-  nama: 'yogi', 
-  energy : 10,
-  makan: function(porsi){
-    this.energy = this.energy + porsi
-    console.log(`selamat datang ${this.nama}, selamat makan!`)
-  }
-}
+// let mahasiswa1 = {
+//   nama: 'yogi', 
+//   energy : 10,
+//   makan: function(porsi){
+//     this.energy = this.energy + porsi
+//     console.log(`selamat datang ${this.nama}, selamat makan!`)
+//   }
+// }
 
-let mahasiswa2 = {
-  nama: 'allea', 
-  energy : 10,
-  makan: function(porsi){
-    this.energy = this.energy + porsi
-    console.log(`selamat datang ${this.nama}, selamat makan!`)
-  }
-}
+// let mahasiswa2 = {
+//   nama: 'allea', 
+//   energy : 10,
+//   makan: function(porsi){
+//     this.energy = this.energy + porsi
+//     console.log(`selamat datang ${this.nama}, selamat makan!`)
+//   }
+// }
 
 // FUNGSI DEKLARASI
+
+
+function mahasiswa(nama, energy){
+  let mahasiswa = {};
+  mahasiswa.nama = nama
+  mahasiswa.energy = energy
+
+  mahasiswa.makan = function (porsi){
+    this.energy += porsi
+    console.log(`halo ${this.nama}, selamat makan`)
+  }
+  return mahasiswa
+}
+let yogi = mahasiswa('Yogi', 14)
 
 // CONTRUCTION FUNCTION
 
